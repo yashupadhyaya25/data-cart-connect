c91b2292502f7d25da935d28b1e6a8d0d33a1541
Yash Upadhyaya <yashupadhyaya01@gmail.com>
2024-04-08 15:22:17 +0530

    Update secret_manager_stack.py

--- resource_stack/secret_manager_stack.py
+++ resource_stack/secret_manager_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
     Stack,
-    aws_secretsmanager as secret_manager
+    aws_secretsmanager as secret_manager,
+    SecretValue
 )
 from constructs import Construct
 import json
@@ -14,30 +15,30 @@ class AwsSecretManagerStack(Stack) :
             id = 'SM_DataCartConnect',
             description= 'All S3 path and other config are store here',
             secret_name= 'SM_DataCartConnect',
-            secret_object_value=secret_manager.SecretProps.secret_string_value("""{
-                                    'product_raw_input_folder': 'raw/product/input/'
-                                    "product_raw_issue_folder": "raw/product/issue/",
-                                    "product_raw_archive_folder": "raw/product/archive/",
-                                    "product_bronze_input_folder": "bronze/product/input/",
-                                    "product_bronze_issue_folder": "bronze/product/issue/",
-                                    "product_bronze_archive_folder": "bronze/product/archive/",
-                                    "product_silver_folder": "silver/product/",
-                                    "cart_raw_input_folder": "raw/cart/input/",
-                                    "cart_raw_issue_folder": "raw/cart/issue/",
-                                    "cart_raw_archive_folder": "raw/cart/archive/",
-                                    "cart_bronze_input_folder": "bronze/cart/input/",
-                                    "cart_bronze_issue_folder": "bronze/cart/issue/",
-                                    "cart_bronze_archive_folder": "bronze/cart/archive/",
-                                    "cart_silver_folder": "silver/cart/",
-                                    "user_raw_input_folder": "raw/user/input/",
-                                    "user_raw_issue_folder": "raw/user/issue/",
-                                    "user_raw_archive_folder": "raw/user/archive/",
-                                    "user_bronze_input_folder": "bronze/user/input/",
-                                    "user_bronze_issue_folder": "bronze/user/issue/",
-                                    "user_bronze_archive_folder": "bronze/user/archive/",
-                                    "user_silver_folder": "silver/user/",
-                                    "raw_bucket" : "datacartconnectraw",
-                                    "bronze_bucket" : "datacartconnectbronze",
-                                    "silver_bucket" : "datacartconnectsilver"
-                                }""")
+            secret_object_value={
+                                    'product_raw_input_folder': SecretValue.unsafe_plain_text('raw/product/input/'),
+                                    'product_raw_issue_folder': SecretValue.unsafe_plain_text('raw/product/issue/'),
+                                    'product_raw_archive_folder': SecretValue.unsafe_plain_text('raw/product/archive/'),
+                                    'product_bronze_input_folder': SecretValue.unsafe_plain_text('bronze/product/input/'),
+                                    'product_bronze_issue_folder': SecretValue.unsafe_plain_text('bronze/product/issue/'),
+                                    'product_bronze_archive_folder': SecretValue.unsafe_plain_text('bronze/product/archive/'),
+                                    'product_silver_folder': SecretValue.unsafe_plain_text('silver/product/'),
+                                    'cart_raw_input_folder': SecretValue.unsafe_plain_text('raw/cart/input/'),
+                                    'cart_raw_issue_folder': SecretValue.unsafe_plain_text('raw/cart/issue/'),
+                                    'cart_raw_archive_folder': SecretValue.unsafe_plain_text('raw/cart/archive/'),
+                                    'cart_bronze_input_folder': SecretValue.unsafe_plain_text('bronze/cart/input/'),
+                                    'cart_bronze_issue_folder': SecretValue.unsafe_plain_text('bronze/cart/issue/'),
+                                    'cart_bronze_archive_folder': SecretValue.unsafe_plain_text('bronze/cart/archive/'),
+                                    'cart_silver_folder': SecretValue.unsafe_plain_text('silver/cart/'),
+                                    'user_raw_input_folder': SecretValue.unsafe_plain_text('raw/user/input/'),
+                                    'user_raw_issue_folder': SecretValue.unsafe_plain_text('raw/user/issue/'),
+                                    'user_raw_archive_folder': SecretValue.unsafe_plain_text('raw/user/archive/'),
+                                    'user_bronze_input_folder': SecretValue.unsafe_plain_text('bronze/user/input/'),
+                                    'user_bronze_issue_folder': SecretValue.unsafe_plain_text('bronze/user/issue/'),
+                                    'user_bronze_archive_folder': SecretValue.unsafe_plain_text('bronze/user/archive/'),
+                                    'user_silver_folder': SecretValue.unsafe_plain_text('silver/user/'),
+                                    'raw_bucket' : SecretValue.unsafe_plain_text('datacartconnectraw'),
+                                    'bronze_bucket' : SecretValue.unsafe_plain_text('datacartconnectbronze'),
+                                    'silver_bucket' : SecretValue.unsafe_plain_text('datacartconnectsilver')
+                                }
         ) 
